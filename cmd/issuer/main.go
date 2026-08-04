7480d9cfc70714dcb647fccf36aac8e1ac407396
hgochev <hgochev@progress.com>
2026-08-04 12:10:29 +0300

    Add create token handler

--- cmd/issuer/main.go
+++ cmd/issuer/main.go
@@ -1,55 +1,29 @@
 package main
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"crypto/x509"
-	"encoding/pem"
-	"fmt"
-	"log"
-	"time"
-
-	"github.com/hgochev/jwt-token-service/internal/token"
+	"github.com/gin-gonic/gin"
+	"github.com/hgochev/jwt-token-service/internal/handlers"
 )
 
 func main() {
-	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	if err != nil {
-		log.Fatalf("generate signing key: %v", err)
-	}
 
-	issuer, err := token.NewIssuer(
-		"https://tokens.example.local",
-		"development-key-1",
-		privateKey,
-		5*time.Minute,
-	)
+	var router *gin.Engine = gin.Default()
 
-	if err != nil {
-		log.Fatalf("create token issuer: %v", err)
-	}
+	router.POST("/jwt/create", handlers.CreateIssuerHandler())
 
-	signedToken, err := issuer.Issue(
-		"system:serviceaccount:payments:payment-api",
-		"orders-api",
-		"orders:read",
-	)
-	if err != nil {
-		log.Fatalf("issue token: %v", err)
-	}
+	router.Run(":8080")
 
-	fmt.Println(signedToken)
+	// fmt.Println(signedToken)
 
-	pubKeyBytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
-	if err != nil {
-		log.Fatalf("marshal public key: %v", err)
-	}
+	// pubKeyBytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	// if err != nil {
+	// 	log.Fatalf("marshal public key: %v", err)
+	// }
 
-	pubKeyPEM := pem.EncodeToMemory(&pem.Block{
-		Type:  "PUBLIC KEY",
-		Bytes: pubKeyBytes,
-	})
+	// pubKeyPEM := pem.EncodeToMemory(&pem.Block{
+	// 	Type:  "PUBLIC KEY",
+	// 	Bytes: pubKeyBytes,
+	// })
 
-	fmt.Println(string(pubKeyPEM))
+	// fmt.Println(string(pubKeyPEM))
 }
